75d8b05a0c6d6a669f65b1f26d1ae13007582445
DESKTOP-PGKRVU2\USER <fomenko_ak21@nuwm.edu.ua>
2025-01-21 19:07:39 +0200

    CretionEntiti 100%

--- Assets/Scripts/CreationMonsters.cs
+++ Assets/Scripts/CreationMonsters.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
-
 public class CreationMonsters : MonoBehaviour
 {
     public GameObject player;
@@ -13,39 +12,46 @@ public class CreationMonsters : MonoBehaviour
     public float maxDistance = 3f;
     public float spawnInterval = 60f;
     private float timer = 0f;
-    public List<GameObject> suitableSpawnPoints = new List<GameObject>();
+    public List<GameObject> suitableSpawnPoints = new List<GameObject>(); 
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        timer += Time.deltaTime;
+
+        // Очищаємо список на початку кожного оновлення
+        suitableSpawnPoints.Clear();
+
         foreach (GameObject obj in EntitiSpaven)
         {
-            timer += Time.deltaTime;
             float distance = Vector3.Distance(player.transform.position, obj.transform.position);
 
             if (distance >= minDistance && distance <= maxDistance)
             {
                 suitableSpawnPoints.Add(obj);
+                
+                
             }
-                if (timer >= spawnInterval)
-                {
-                    timer = 0f;
-                    SpawnEnemy();
-                }
         }
-    }
-    
-    void SpawnEnemy()
-    {
         if (suitableSpawnPoints.Count > 0)
         {
-            // Вибираємо випадкову точку зі списку відповідних точок
+            if (timer >= spawnInterval)
+            {
+                timer = 0f;
+                SpawnEnemy();
+            }
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        // Вибираємо випадкову точку зі списку відповідних точок
             int randomIndex = Random.Range(0, suitableSpawnPoints.Count);
             Vector3 spawnPosition = suitableSpawnPoints[randomIndex].transform.position;
 
             // Спавнюємо ворога
             GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
-        }
+            Debug.Log("Випадкове число: " + randomIndex);
     }
-    
+
 }
